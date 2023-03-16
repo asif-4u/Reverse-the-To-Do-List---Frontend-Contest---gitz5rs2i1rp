8c0e368ac6852f256682d2b591832fabbd7fc04e
ASHIQUE IQBAL <106628177+asif-4u@users.noreply.github.com>
2023-03-16 20:08:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,52 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
-function ToDo() {
-  return (<tr>
-    <td>
-      <p>id</p>
-    </td>
-    <td>
-      <input />
-    </td>
-    <td>
-      <p>createdAt</p>
-    </td>
-  </tr>)
+function ToDo(props) {
+  return (
+    <tr>
+      <td>
+        <p>{props.id}</p>
+      </td>
+      <td>
+        <input />
+      </td>
+      <td>
+        <p>{props.createdAt}</p>
+      </td>
+    </tr>
+  );
 }
 
 function App() {
-  const [todos, setTodos] = useState([{
-    id: 'todo1',
-    createdAt: '20:30',
-  }, {
-    id: 'todo2',
-    createdAt: '18:00',
-  }
+  const [todos, setTodos] = useState([
+    {
+      id: "todo1",
+      createdAt: "20:30",
+    },
+    {
+      id: "todo2",
+      createdAt: "18:00",
+    },
   ]);
+  const reverseHandler = () => {
+    const arr = [];
+    arr[0] = todos[1];
+    arr[1] = todos[0];
+    setTodos(arr);
+  };
 
   return (
     <div id="main">
-      <button>Reverse</button>
+      <button onClick={reverseHandler}>Reverse</button>
       <table>
         <tbody>
+          {todos.map((el) => {
+            return <ToDo key={el.id} id={el.id} createdAt={el.createdAt} />;
+          })}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
-
 
 export default App;
